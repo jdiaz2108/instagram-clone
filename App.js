9e1294e5f77bb82e5e2dd874bcd53118a46d573f
jdiaz2108 <jdiaz2108@hotmail.com>
2021-07-03 11:23:05 -0500

    first commit

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+import LandingScreen from './components/auth/Landing'
+
+const Stack = createStackNavigator()
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouterName="Landing">
+				<Stack.Screen name="landing" component={LandingScreen} options={{ HeaderShown: false}} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
